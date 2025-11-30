d9f70a474be47be13f81066f7a0fd7a5aace5108
seethalakshmivenkatesh <seethavenkatesh79@gmail.com>
2025-11-30 22:46:40 +0530

    Create leetcode1382.java

---
+++ leetcode1382.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public TreeNode balanceBST(TreeNode root) {
+        inorder(root);
+        return buildBST( 0, list.size() - 1);
+
+    }
+    public void inorder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+    TreeNode buildBST(int left, int right){
+        if(left > right) return null;
+        int mid = (left+right) / 2;
+        TreeNode root = new TreeNode(list.get(mid));
+        root.left = buildBST(left, mid - 1);
+        root.right = buildBST(mid+1, right);
+        return root;
+    }
+    
+}

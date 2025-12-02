f2cf28a659d4d1db7b5c44acfc0e423d12f1cd92
seethalakshmivenkatesh <seethavenkatesh79@gmail.com>
2025-12-02 22:56:59 +0530

    Create lc_1845.java

---
+++ lc_1845.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+class SeatManager {
+    private PriorityQueue<Integer> pq;
+    public SeatManager(int n) {
+        pq = new PriorityQueue<>();
+        for(int i = 1; i<=n; i++){
+            pq.offer(i);
+        }
+    }
+    
+    public int reserve() {
+        int num = pq.poll();
+        return num;
+    }
+    
+    public void unreserve(int seatNumber) {
+        pq.offer(seatNumber);
+    }
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */

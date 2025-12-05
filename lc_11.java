83a1de3a671b55a845ffe13265912d54876abe02
seethalakshmivenkatesh <seethavenkatesh79@gmail.com>
2025-12-05 23:16:15 +0530

    Create lc_11.java

---
+++ lc_11.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int ans = -1;
+        while(l < r){
+            int w = r - l;
+            int h = Math.min(height[l], height[r]);
+            ans = Math.max(ans, (w * h));
+            if(height[l] < height[r]){
+                 l++;
+            }else{
+            r--;
+            }
+        }
+        return ans;
+    }
+}
